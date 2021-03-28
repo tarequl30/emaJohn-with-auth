6455e8850d596040f79ebc4fd2f7eaffa2df43bb
Tareq <tarequl30@gmail.com>
2021-03-28 18:29:11 +0600

    first commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import fakeData from '../../fakeData';
+
+
+
+const Inventory = () => {
+    const handleAddPd = () => {
+        const product = {}
+        fetch('http://localhost:5000/addProduct', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(product)
+        })
+        
+    }
+    return (
+        <div>
+            <form action="">
+                <p><span>name:</span><input type="text"/></p>
+                <p><span>price:</span><input type="text"/></p>
+                <p><span>quantity:</span><input type="text"/></p>
+                <p><span>image:</span><input type="file"/></p>
+                <button onClick={handleAddPd}>add pd</button>
+            </form>
+        </div>
+    );
+};
+
+export default Inventory;
